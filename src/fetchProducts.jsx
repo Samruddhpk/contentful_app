a2b27ed405818bcf537612d8540902fb1051ec29
Samruddh pk <107286956+Samruddhpk@users.noreply.github.com>
2024-04-06 16:31:59 +0530

    first-commit

---
+++ src/fetchProducts.jsx
@@ -0,0 +1,41 @@
+// this code written in node as we r using it in front end slightly code changes
+
+import { createClient } from "contentful";
+import { useEffect, useState } from "react";
+
+const client = createClient({
+  space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+  environment: "master",
+  accessToken: import.meta.env.VITE_CONTENTFUL_API_KEY,
+});
+
+// customHook
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({
+        content_type: "projects",
+      });
+      // parse the data as it is a complex api structure
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return { title, url, id, img };
+      });
+      setProjects(projects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return { loading, projects };
+};
